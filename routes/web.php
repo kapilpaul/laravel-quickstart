5f3a9fb0f1e9f923348a8a9bd1b6069586baca23
Kapil Paul <kapilpaul007@gmail.com>
2018-04-25 11:35:39 +0600

    login, adminUser controllers and routes added

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,39 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::middleware(['visitors'])->group(function () {
+
+    /* Register Routes */
+
+//    Route::get('/register', 'RegistrationController@register')->name('register');
+//    Route::post('/register', 'RegistrationController@postRegister')->name('postRegister');
+
+    /* Login Routes */
+
+    Route::get('/login', 'Login\LoginController@login')->name('login');
+    Route::post('/login', 'Login\LoginController@postLogin')->name('postLogin');
+
+
+    /* Activation by email Routes */
+
+    Route::get('/activation/{email}/{activationcode}', 'Login\ActivationController@activateUser');
+
+    /* Forgot Password Routes */
+
+    Route::get('/forgotpassword', 'Login\ForgetPasswordController@forgotPasword')->name('forgotpassword');
+    Route::post('/forgotpassword', 'Login\ForgetPasswordController@postForgotPassword')->name('postForgotpassword');
+
+    /* Reset Routes */
+
+    Route::get('/reset/{email}/{code}', 'Login\ForgetPasswordController@resetPassword')->name('resetPassword');
+    Route::post('/reset/{email}/{code}', 'Login\ForgetPasswordController@postResetPassword')->name('postResetPassword');
+
+});
+
+
+Route::middleware(['authcheck'])->group(function () {
+    
+
+});
